25ccd8cc5bfa38816fc9560017f8cc4300a98ac1
Akanksharani85 <akanksharani5190@gmail.com>
2025-08-11 11:48:17 +0530

    qwe

--- src/main/java/com/example/ecommerce/controller/AdminController.java
+++ src/main/java/com/example/ecommerce/controller/AdminController.java
@@ -45,7 +45,7 @@ public class AdminController {
     ) {
         try {
 // File system path (outside classpath)
-            String uploadDir = "D:\\lnctu\\ecommerce\\src\\main\\resources\\static\\images";
+            String uploadDir = "C:\\ecommerce\\src\\main\\resources\\static\\images";
             File uploadFolder = new File(uploadDir);
             if (!uploadFolder.exists()) {
                 uploadFolder.mkdirs(); // create folder if it doesn't exist
